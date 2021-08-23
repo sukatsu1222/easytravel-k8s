72313948a4097308107bb0a1cd74d69aee6bb28b
Katsuyoshi <katsuyoshi.sumida@dynatrace.com>
2021-08-23 22:38:31 +0900

    Update deploy-easytravel.sh

--- deploy-easytravel.sh
+++ deploy-easytravel.sh
@@ -2,7 +2,16 @@
 
 MANIFESTS_DIR=$(cd $(dirname $0) && pwd)/manifests
 
-kubectl apply -f ${MANIFESTS_DIR}/easytravel-namespace.yaml
-kubectl apply -f ${MANIFESTS_DIR}/backend/
-kubectl apply -f ${MANIFESTS_DIR}/frontend/
-kubectl apply -f ${MANIFESTS_DIR}/loadgen/
+if type microk8s > /dev/null 2>&1; then
+    COMMAND="microk8s kubectl"
+elif type kubectl > /dev/null 2>&1; then
+    COMMAND="kubectl"
+else
+    echo "Not found kubectl or microk8s command!"
+    exit 1
+fi
+
+${COMMAND} apply -f ${MANIFESTS_DIR}/easytravel-namespace.yaml
+${COMMAND} apply -f ${MANIFESTS_DIR}/backend/
+${COMMAND} apply -f ${MANIFESTS_DIR}/frontend/
+${COMMAND} apply -f ${MANIFESTS_DIR}/loadgen/
