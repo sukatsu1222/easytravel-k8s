1b39463d43be44b9f9bc65ae536c2341c1d936c9
Katsuyoshi <katsuyoshi.sumida@dynatrace.com>
2021-11-19 13:52:53 +0900

    Update

--- deploy-easytravel.sh
+++ deploy-easytravel.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+DEPLOY_LOADGEN=${DEPLOY_LOADGEN:-true}
+
 MANIFESTS_DIR=$(cd $(dirname $0) && pwd)/manifests
 
 if type microk8s > /dev/null 2>&1; then
@@ -14,4 +16,7 @@ fi
 ${COMMAND} apply -f ${MANIFESTS_DIR}/easytravel-namespace.yaml
 ${COMMAND} apply -f ${MANIFESTS_DIR}/backend/
 ${COMMAND} apply -f ${MANIFESTS_DIR}/frontend/
-${COMMAND} apply -f ${MANIFESTS_DIR}/loadgen/
+
+if [[ "$DEPLOY_LOADGEN" == "true" ]]; then
+    ${COMMAND} apply -f ${MANIFESTS_DIR}/loadgen/
+fi
